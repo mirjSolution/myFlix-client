fde61faa830dc71d0d54287318987b4719f479ab
mirjSolution <ricojohndatoon23@gmail.com>
2020-11-19 13:58:18 -0500

    Fix routing

--- src/components/routing/routes.jsx
+++ src/components/routing/routes.jsx
@@ -10,7 +10,6 @@ import DirectorView from '../director-view/director-view';
 import LoginView from '../login-view/login-view';
 import ProfileView from '../profile-view/profile-view';
 import RegistrationView from '../registration-view/registratrion-view';
-import PrivateRoute from '../routing/privateroute';
 
 const Routes = ({ auth }) => {
   return (
@@ -18,16 +17,12 @@ const Routes = ({ auth }) => {
       {!auth ? <Redirect to='/login' /> : <Redirect to='/' />}
       <Switch>
         <Route exact path='/login' component={LoginView} />
-        <PrivateRoute exact path='/' component={MovieList} />
-        <PrivateRoute exact path='/register' component={RegistrationView} />
-        <PrivateRoute exact path='/profile' component={ProfileView} />
-        <PrivateRoute exact path='/movies/:title' component={MovieView} />
-        <PrivateRoute
-          exact
-          path='/movies/genre/:genreName'
-          component={GenreView}
-        />
-        <PrivateRoute
+        <Route exact path='/' component={MovieList} />
+        <Route exact path='/register' component={RegistrationView} />
+        <Route exact path='/profile' component={ProfileView} />
+        <Route exact path='/movies/:title' component={MovieView} />
+        <Route exact path='/movies/genre/:genreName' component={GenreView} />
+        <Route
           exact
           path='/movies/director/:directorName'
           component={DirectorView}
